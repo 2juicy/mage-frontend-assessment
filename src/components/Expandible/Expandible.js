e2e4e31a5e145ea3223e0976f266f7eec483d1c0
JohnnyC <j0hnnywchiang@gmail.com>
2021-07-24 22:39:46 -0700

    warning removed

--- src/components/Expandible/Expandible.js
+++ src/components/Expandible/Expandible.js
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function Expandible({ contents }) {
   return (
-    <>
-      <br />
+    <tbody>
       {contents.map((content, index) => (
-        <tr>
+        <tr key={index}>
           <td>Test {index + 1}:</td>
           <td>{content}%</td>
         </tr>
       ))}
-    </>
+    </tbody>
   );
 }
--- src/components/Grades/Grades.js
+++ src/components/Grades/Grades.js
@@ -4,10 +4,12 @@ import Expandible from "../Expandible/Expandible";
 export default function Grades({ grades, findAverage }) {
   return (
     <table className="grades">
-      <tr>
-        <th>Average:&nbsp;</th>
-        <th>{findAverage(grades)}%</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Average:&nbsp;</th>
+          <th>{findAverage(grades)}%</th>
+        </tr>
+      </thead>
       <Expandible contents={grades} />
     </table>
   );
