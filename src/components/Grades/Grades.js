a64d493ba288a164dc7c6dacadf31de73bc54339
JohnnyC <j0hnnywchiang@gmail.com>
2021-07-26 18:41:27 -0700

    memo grades

--- src/components/Grades/Grades.js
+++ src/components/Grades/Grades.js
@@ -1,12 +1,25 @@
 import "./Grades.css";
+import { useMemo, useCallback } from "react";
 
-export default function Grades({ grades, findAverage, children }) {
+export default function Grades({ grades, children }) {
+  const findAverage = useCallback(arr => {
+    console.log("render");
+    return arr.reduce(function (avg, value, _, { length }) {
+      return avg + value / length;
+    }, 0);
+  }, []);
+
+  const averageGrade = useMemo(
+    () => findAverage(grades),
+    [grades, findAverage]
+  );
+
   return (
     <table className="grades">
       <thead>
         <tr>
           <th>Average:&nbsp;</th>
-          <th>{findAverage(grades)}%</th>
+          <th>{averageGrade}%</th>
         </tr>
       </thead>
       {children}
