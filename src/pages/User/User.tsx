f3d21e62021897f53ed383f6750d53dc4ae54726
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-22 07:46:33 -0700

    Update typescript

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import UserInfo from "../../components/UserInfo/UserInfo";
 import Searchbar from "../../components/Searchbar/Searchbar";
 
-function fetchData(url) {
+function fetchData(url: string) {
   return fetch(url)
     .then(response => {
       if (response.ok) return response.json();
@@ -15,9 +15,9 @@ function fetchData(url) {
 export default function User() {
   // Stored fetch request data.
   const URL = "https://api.hatchways.io/assessment/students";
-  const [results, setResults] = useState([]);
+  const [results, setResults]: any[] = useState([]);
   // Search plus filtered results and form state.
-  const [filter, setFilter] = useState([]);
+  const [filter, setFilter]: any[] = useState([]);
   const [form, setForm] = useState({
     name: "",
     tag: "",
@@ -26,17 +26,19 @@ export default function User() {
   useEffect(() => {
     fetchData(URL).then(data => {
       setResults(data.students);
-      data.students.forEach(student => {
-        student.show = false;
-        student.tags = [];
-      });
+      data.students.forEach(
+        (student: { show: boolean; tags: Array<string> }) => {
+          student.show = false;
+          student.tags = [];
+        }
+      );
       setFilter(data.students);
     });
   }, []);
 
-  function filterResults(name, tag) {
+  function filterResults(name: string, tag: string) {
     // We use this function to see a substring or string exists in target string.
-    function hasWord(target, string) {
+    function hasWord(target: string, string: string) {
       return (
         target
           .toLowerCase()
@@ -45,16 +47,16 @@ export default function User() {
       );
     }
     // First we filter by name by looping through.
-    const filterName = [];
-    results.forEach(result => {
+    const filterName: any[] = [];
+    results.forEach((result: { firstName: string; lastName: string }) => {
       if (hasWord(result.firstName + result.lastName, name))
         filterName.push(result);
     });
     // If a tag parameter exists we then continue to filter by tag.
     if (tag.length > 0) {
-      const filterTag = [];
+      const filterTag: any[] = [];
       filterName.forEach(result => {
-        result.tags.some(data => {
+        result.tags.some((data: string) => {
           return hasWord(data, tag) ? filterTag.push(result) : false;
         });
       });
@@ -63,21 +65,22 @@ export default function User() {
     return setFilter(filterName);
   }
 
-  function handleForm(e) {
+  function handleForm(e: React.ChangeEvent<HTMLInputElement>) {
     setForm({
       ...form,
       [e.target.name]: e.target.value,
     });
   }
 
-  function handleExpand(index) {
+  function handleExpand(index: any) {
+    console.log(index);
     // We clone the data to remove references before we set the state to avoid mutating the state.
     let clonedData = JSON.parse(JSON.stringify(filter));
     clonedData[index].show = !clonedData[index].show;
     setFilter([...clonedData]);
   }
 
-  function addTag(tag, index) {
+  function addTag(tag: any, index: any) {
     if (!filter[index].tags.includes(tag)) {
       filter[index].tags = [...filter[index].tags, tag];
       setFilter([...filter]);
@@ -86,7 +89,7 @@ export default function User() {
 
   return (
     <div className="paper">
-      {Object.keys(form).map(key => (
+      {(Object.keys(form) as Array<keyof typeof form>).map(key => (
         <Searchbar
           key={key}
           placeholder={`Search by ${key}`}
